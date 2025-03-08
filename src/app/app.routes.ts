324e32e1c99eaa18611484c5580f4ab031b35959
OmarMoralesdev <matymv8288@gmail.com>
2025-03-07 19:02:42 -0600

    repo y graficas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { GraficaComponent } from '../app/componets/grafica/grafica.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {
+        path: '',
+        component: GraficaComponent
+    }
+];
