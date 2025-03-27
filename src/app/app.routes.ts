2494a9d619b6f53f007ecf31624424d90e8d2eaa
RubiLanda <rubivalenzuela8528@gmail.com>
2025-03-27 16:31:47 -0600

    habitaciones

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -14,6 +14,7 @@ import{HabitacionesComponent} from './views/user/habitaciones/habitaciones.compo
 import { EditarContrasenaComponent } from './views/auth/editar-contrasena/editar-contrasena.component';
 import { EditarPerfilComponent } from './views/auth/editar-perfil/editar-perfil.component';
 import { ReporteDiarioComponent } from './views/user/reporte/reporte-diario/reporte-diario.component';
+import { AltaHabitacionComponent } from './views/user/alta-habitacion/alta-habitacion.component';
 
 export const routes: Routes = [
 
@@ -85,6 +86,11 @@ export const routes: Routes = [
         //RUTA PARA VER MIS HABITACIONES 
         path: 'misHabitaciones',
         component: HabitacionesComponent
+    },
+    {
+        //RUTA PARA DAR DE ALTA HABITACION
+        path: 'nuevaHabitacion',
+        component: AltaHabitacionComponent
     },
     {
         path: 'editar-contraseña',
--- src/app/core/services/habitaciones.service.ts
+++ src/app/core/services/habitaciones.service.ts
@@ -2,17 +2,26 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Habitacion } from '../models/habitacion';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HabitacionesService {
 
-  private url = 'http://127.0.0.1:8000/api/v1/habitacion'
+  private url = environment.apiUrl+'/habitacion'
 
   constructor(private http:HttpClient) { }
 
   getHabitacion(): Observable<{data: Habitacion[]}> {
     return this.http.get<{data: Habitacion[]}>(this.url);
   }
+
+  postHabitacion(Habitacion: Habitacion):Observable<{msg:string; data:Habitacion}>{
+    return this.http.post<{msg: string; data:Habitacion}>(this.url,Habitacion);
+  }
+
+  deleteHabitacion(id: number): Observable<Habitacion> {
+    return this.http.delete<Habitacion>(`${this.url}/${id}`);
+}
 }
